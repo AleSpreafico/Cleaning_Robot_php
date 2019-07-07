446db56b192e8daeb84de3e3f2e56d67642376f0
AleSpreafico <ale.spreafico@yandex.com>
2019-07-07 15:31:04 +0200

    finalizing

--- index.php
+++ index.php
@@ -15,15 +15,5 @@ $json_input = file_get_contents($input);
 
 $json = json_decode($json_input, true);
 
-// TODO: remember to validate!!
-
 $user_cleaning_robot = new MyQCleaningRobot($json, $output);
 $user_cleaning_robot->start();
-
-
-// var_dump($json);
-
-
-// echo '<pre>' . print_r($json, true) . '</pre>';
-
-// echo $json;
